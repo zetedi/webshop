8d05560b804fb3329cfb72d9524452e78f8e707b
Zoltan Etedi <zetedi@gmail.com>
2021-02-23 22:00:51 +0100

    Delete Product.

---
+++ sick-fits/frontend/components/DeleteProduct.js
@@ -0,0 +1,41 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+function update(cache, payload) {
+  console.log(payload);
+  console.log('update function after delete');
+  cache.evict(cache.identify(payload.data.deleteProduct));
+}
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: {
+      id,
+    },
+    update,
+  });
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        if (confirm('Are you sure you want to delete it?')) {
+          // delete
+          console.log('delete');
+          deleteProduct().catch((err) => alert(err.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
